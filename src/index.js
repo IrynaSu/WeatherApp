cc2a5cd2799caaefe0949320785279e3571e1d4d
IrynaSu <110418949+IrynaSu@users.noreply.github.com>
2022-08-03 20:25:24 +0300

    added link, code optimization

--- src/index.js
+++ src/index.js
@@ -21,8 +21,25 @@ if (minutes < 10) {
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = `${day} ${hours}:${minutes}`;
 
-function showCurrentPositionInfo() {
-  navigator.geolocation.getCurrentPosition(showCurrentPosition);
+function showTemp(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
 function showCurrentPosition(position) {
@@ -31,72 +48,23 @@ function showCurrentPosition(position) {
   let apiKey = "3f8e63b0ac8703d0297945f6602e377d";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showCurrentPositionTemp);
+  axios.get(apiUrl).then(showTemp);
 }
 
-function showCurrentPositionTemp(response) {
-  let cityName = response.data.name;
-  let cityNameElement = document.querySelector("#city-name");
-  cityNameElement.innerHTML = cityName;
-
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = temperature;
-
-  let feelsLike = Math.round(response.data.main.feels_like);
-  let feelsLikeElement = document.querySelector("#feels-like");
-  feelsLikeElement.innerHTML = feelsLike;
-
-  let description = response.data.weather[0].main;
-  let descriptionElement = document.querySelector("#weather-description");
-  descriptionElement.innerHTML = description;
-
-  let humidity = response.data.main.humidity;
-  let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = humidity;
-
-  let wind = Math.round(response.data.wind.speed);
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = wind;
+function showCurrentPositionInfo() {
+  navigator.geolocation.getCurrentPosition(showCurrentPosition);
 }
-
-showCurrentPositionInfo();
 
 function showCity(event) {
   event.preventDefault();
   let citySearch = document.querySelector("#city-search_field");
-  let cityNameSearch = citySearch.value;
   let apiKey = "3f8e63b0ac8703d0297945f6602e377d";
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameSearch}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemp);
 }
 
-function showTemp(response) {
-  let cityName = response.data.name;
-  let cityNameElement = document.querySelector("#city-name");
-  cityNameElement.innerHTML = cityName;
-
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = temperature;
-
-  let feelsLike = Math.round(response.data.main.feels_like);
-  let feelsLikeElement = document.querySelector("#feels-like");
-  feelsLikeElement.innerHTML = feelsLike;
-
-  let description = response.data.weather[0].main;
-  let descriptionElement = document.querySelector("#weather-description");
-  descriptionElement.innerHTML = description;
-
-  let humidity = response.data.main.humidity;
-  let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = humidity;
-
-  let wind = Math.round(response.data.wind.speed);
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = wind;
-}
+showCurrentPositionInfo();
 
 let citySearch = document.querySelector("#city-search");
 citySearch.addEventListener("submit", showCity);
